67790cd31415560152038096db86367fdcb6cc96
MacTwister <MacTwister@users.noreply.github.com>
2024-06-04 08:19:41 +0200

    Style header with title (upstream update)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,12 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 export const Navbar = ({ isCartAccessible, cartItems, toggleCartModal }) => {
     return (
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-10 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+            
+        <nav className="bg-gray-900 dark:bg-gray-900 fixed w-full z-10 top-0 start-0 border-b border-gray-600 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src={fabLogo} className="w-10 h-10" alt="Flowbite logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Fab24</span>
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap text-white dark:text-white">FAB24 Inventory</span>
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-3 rtl:space-x-reverse ">
                     {isCartAccessible && (
